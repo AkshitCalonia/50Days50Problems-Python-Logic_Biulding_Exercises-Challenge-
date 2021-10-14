35ce79110de90d7f0164281871d54b838e9924d9
Akshit Calonia <76471555+AkshitCalonia@users.noreply.github.com>
2021-10-14 22:29:30 +0530

    Add files via upload

---
+++ P-#31.py
@@ -0,0 +1,25 @@
+#Sales by Match
+'''
+Given a list of integers representing the color of each sock, determine how many pairs of socks with matching colors there are. For example, there are 7 socks with colors [1, 2, 1, 2, 1, 3, 2]. There is one pair of color 1 and one of color 2. There are three odd socks left, one of each color. The number of pairs is 2.
+
+Create a function that returns an integer representing the number of matching pairs of socks that are available.
+
+Examples
+sock_merchant([10, 20, 20, 10, 10, 30, 50, 10, 20]) ➞ 3
+
+sock_merchant([50, 20, 30, 90, 30, 20, 50, 20, 90]) ➞ 4
+
+sock_merchant([]) ➞ 0
+'''
+
+def dups(l):
+    lis = sorted(l)
+    recs = 0
+    for i in set(lis):
+        c = lis.count(i)
+        recs += (c//2)
+
+    return recs
+
+print(dups([50, 20, 30, 90, 30, 20, 50, 20, 90]))
+
