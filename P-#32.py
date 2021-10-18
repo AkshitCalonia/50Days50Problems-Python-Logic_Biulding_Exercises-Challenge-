6f057dabbcd27a6f7faf4aacdadaa3e119a2fe89
Akshit Calonia <76471555+AkshitCalonia@users.noreply.github.com>
2021-10-18 21:22:43 +0530

    Add files via upload

---
+++ P-#32.py
@@ -0,0 +1,54 @@
+#Tallest Skyscraper
+'''
+A city skyline can be represented as a 2-D list with 1s representing buildings. In the example below, the height of the tallest building is 4 (second-most right column).
+
+[[0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 1, 0],
+[0, 0, 1, 0, 1, 0],
+[0, 1, 1, 1, 1, 0],
+[1, 1, 1, 1, 1, 1]]
+Create a function that takes a skyline (2-D list of 0's and 1's) and returns the height of the tallest skyscraper.
+
+Examples
+tallest_skyscraper([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 1, 1, 0],
+  [1, 1, 1, 1]
+]) ➞ 3
+
+tallest_skyscraper([
+  [0, 1, 0, 0],
+  [0, 1, 0, 0],
+  [0, 1, 1, 0],
+  [1, 1, 1, 1]
+]) ➞ 4
+
+tallest_skyscraper([
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [1, 1, 1, 0],
+  [1, 1, 1, 1]
+]) ➞ 2
+'''
+
+def building_height(ls):
+    recs = []
+    for i in ls:
+        for item in i:
+            if item == 1:
+                k = i.index(item)
+                recs.append(k)
+                i[i.index(item)] = " "
+    recs2 = []
+    for m in set(recs):
+        recs2.append(recs.count(m))
+
+    return max(recs2)
+
+print(building_height([
+[0, 0, 0, 0, 1, 0],
+[0, 0, 0, 0, 1, 0],
+[0, 0, 1, 0, 1, 0],
+[0, 1, 1, 1, 1, 0],
+[1, 1, 1, 1, 1, 1]]))
