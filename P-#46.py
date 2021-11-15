008878c33687b7b643e1791c7d9e163beb32a88a
Akshit Calonia <76471555+AkshitCalonia@users.noreply.github.com>
2021-11-15 22:34:32 +0530

    Add files via upload

---
+++ P-#46.py
@@ -0,0 +1,23 @@
+#Majority Vote
+'''
+Create a function that returns the majority vote in a list. A majority vote is an element that occurs > N/2 times in a list (where N is the length of the list).
+
+Examples
+majority_vote(["A", "A", "B"]) ➞ "A"
+majority_vote(["A", "A", "A", "B", "C", "A"]) ➞ "A"
+majority_vote(["A", "B", "B", "A", "C", "C"]) ➞ None
+
+Notes
+The frequency of the majority element must be strictly greater than 1/2.
+If there is no majority element, return None.
+If the list is empty, return None.
+'''
+
+def maj_vote(ls):
+    for i in set(ls):
+        if ls.count(i)> (len(ls)/2):
+            return i
+        else:
+            return None
+
+print(maj_vote(["A", "A", "B"]))
